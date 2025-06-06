aa9817644a95662967bd300ee5f1dc994770ab4b
iabhinavdubey <iabhinavkumar3@gmail.com>
2025-06-06 17:03:47 +0530

    made some new changes

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const connectDB = require("./config/db");
 
 
 
-app.use(cors({
-  origin: 'https://cms-frontend-six-steel.vercel.app', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+
 
 
 const app = express();
@@ -19,7 +15,13 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: 'https://cms-frontend-six-steel.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 
 
